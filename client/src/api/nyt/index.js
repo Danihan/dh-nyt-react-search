842fdfa669c0a1a3b7b97d18986cd268334fb309
Dan Linehan <wagist@gmail.com>
2018-10-31 00:41:29 +0100

    Add semicolons

--- client/src/api/nyt/index.js
+++ client/src/api/nyt/index.js
@@ -1,13 +1,13 @@
-const API_KEY = process.env.REACT_APP_NYT_API_KEY
+const API_KEY = process.env.REACT_APP_NYT_API_KEY;
 
 async function search(topic, startYear, endYear) {
-  const query = `?q=${encodeURIComponent(topic)}&begin_date=${startYear}0101&endDate=${endYear}0101&fl=web_url,headline,pub_date&api-key=${API_KEY}`
-  const res = await fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json${query}`)
-  const resp = await res.json()
+  const query = `?q=${encodeURIComponent(topic)}&begin_date=${startYear}0101&endDate=${endYear}0101&fl=web_url,headline,pub_date&api-key=${API_KEY}`;
+  const res = await fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json${query}`);
+  const resp = await res.json();
 
-  return resp
+  return resp;
 }
 
 export default {
   search
-}
+};
