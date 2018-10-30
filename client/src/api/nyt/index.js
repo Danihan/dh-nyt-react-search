5fff71190aec90fb9b8db14129999c11d9cdb58c
Dan Linehan <wagist@gmail.com>
2018-10-31 00:33:48 +0100

    Fix env variable

--- client/src/api/nyt/index.js
+++ client/src/api/nyt/index.js
@@ -1,4 +1,4 @@
-const API_KEY = process.env.NYT_API_KEY
+const API_KEY = process.env.REACT_APP_NYT_API_KEY
 
 async function search(topic, startYear, endYear) {
   const query = `?q=${encodeURIComponent(topic)}&begin_date=${startYear}0101&endDate=${endYear}0101&fl=web_url,headline,pub_date&api-key=${API_KEY}`
